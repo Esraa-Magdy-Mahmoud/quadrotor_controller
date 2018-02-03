06a12b3af780d67dce7a9c44cb93f6f413e95ab8
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-02-03 20:17:19 +0200

    Modified CMakeLists

--- include/quadrotor_controller/QuadRotorController.h
+++ include/quadrotor_controller/QuadRotorController.h
@@ -3,7 +3,9 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include "quadrotor_controller/Controller.h"
+#include <gazebo_msgs/ModelState.h>
+#include <string.h>
+#include <geometry_msgs/PoseStamped.h>
 
 
 namespace quadrotor_controller
@@ -18,15 +20,41 @@ namespace quadrotor_controller
         
 		//---Methods definition---//
 		bool readParameters();
-	
+		void poseCallback(const geometry_msgs::PoseStampedConstPtr &posemsg);
+	    void pidController(double pose_z,double time_now);
+			
 
         //---
+		double thrust_;
+			
+		double kpz_;
+		double kdz_;
+        double momentx_;
+		double momenty_;
+		double momentz_;
 
+			//Target point
+		double pos_x_target;
+		double pos_y_target; 
+    	double pos_z_target = 10.0;
+			//
+		double last_error_ = 0.0;
+		double error_sum_  = 0.0;
 
+		double start_time_;
+		double last_time_  = 0.0;
+        double timenow_;
 
         
 		ros::NodeHandle nodeHandle_;
-        //Controller controller_;
+		ros::Subscriber poseSub_;
+		
+       
+
+		//--QuadStates---//
+		double pose_x_;
+		double pose_y_;
+		double pose_z_;
 
 
 
--- src/QuadRotorController.cpp
+++ src/QuadRotorController.cpp
@@ -9,7 +9,14 @@ namespace quadrotor_controller
             ROS_ERROR("Could not read parameters.");
             ros::requestShutdown();
         }
-         ROS_INFO("Successfully launched node.");
+
+        //--subscribers--//
+        poseSub_ = nodeHandle_.subscribe("/pose", 10,&QuadRotorController::poseCallback, this);
+        ROS_INFO("Successfully launched node.");
+        timenow_ = ros::Time::now().toSec();
+        
+       
+
     }
 
     QuadRotorController::~QuadRotorController()
@@ -20,6 +27,32 @@ namespace quadrotor_controller
 
     bool QuadRotorController::readParameters()
     {
+        return true;
+
+    }
+
+    void QuadRotorController::poseCallback(const geometry_msgs::PoseStampedConstPtr &posemsg)
+    {
+         pose_x_ = posemsg->pose.position.x;
+         pose_y_ = posemsg->pose.position.y;
+         pose_z_ = posemsg->pose.position.z;
+
+    }
+    void QuadRotorController::pidController(double pose_z,double time_now)
+    {
+        double dt = time_now - last_time_;
+        if (dt == 0) 
+        {
+            thrust_ = 0.0;
+        }
+        last_time_ = time_now ;
+        double error_z   = pose_z_ - pose_z;
+        error_sum_ += (error_z*dt) ;
+        double derror_z  = error_z - last_error_;
+        last_error_ = error_z;
+
+        thrust_ = (kpz_*error_z)+(kdz_*derror_z);
+       
 
     }
     
