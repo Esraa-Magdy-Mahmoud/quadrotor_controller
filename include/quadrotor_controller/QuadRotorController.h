9663c9d63e0a820bd35021bf76733280d252db29
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-02-05 12:43:27 +0200

    completed 3D hover controller

--- include/quadrotor_controller/QuadRotorController.h
+++ include/quadrotor_controller/QuadRotorController.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3Stamped.h>
 #include <gazebo_msgs/ModelState.h>
 #include <string.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -21,33 +22,76 @@ namespace quadrotor_controller
 		//---Methods definition---//
 		bool readParameters();
 		void poseCallback(const geometry_msgs::PoseStampedConstPtr &posemsg);
+		void eulerCallback(const geometry_msgs::Vector3StampedConstPtr &eulermsg);
 	    void pidController(double pose_z,double time_now);
-			
+		//---
+ 
+        //-- nodehandle
+		ros::NodeHandle nodeHandle_;
 
-        //---
+		
+        //-- states sub
+		ros::Subscriber poseSub_;
+		ros::Subscriber eulerSub_;
+        //--
+		
+		ros::Publisher  velPub_;
+		geometry_msgs::Twist velmsg_; 
+
+
+
+		//-- pid gains--//
+		double kpz_ = 10;
+		double kdz_ = 0.5;
+		double kpx_ = 0.3;
+		double kdx_ = 0.5;
+		double kpy_ = 0.7;
+		double kdy_ = 0.5;
+		double kp_roll = 15;
+		double kd_roll = 0.4;
+		double kp_pitch = 0.4;
+		double kd_pitch = 0.4;
+		double kp_yaw = 0.7;
+		double kd_yaw = 0.7;
+
+		//--
 		double thrust_;
-			
-		double kpz_;
-		double kdz_;
-        double momentx_;
+		double momentx_;
 		double momenty_;
 		double momentz_;
 
-	   //Target point position
-		double pos_x_target;
-		double pos_y_target; 
+	   //--Target point position
+		double pos_x_target = 10.0;
+		double pos_y_target = 10.0; 
     	double pos_z_target = 10.0;
-	  //
-		double last_error_ = 0.0;
-		double error_sum_  = 0.0;
+		double yaw_target = 0.0;
+		double dyaw_target= 0.0;
 
-		double start_time_;
+	  //----
+		double last_errorx_ = 0.0;
+		double error_sumx_  = 0.0;
+
+	    double last_errory_ = 0.0;
+		double error_sumy_  = 0.0;
+
+		double last_errorz_ = 0.0;
+		double error_sumz_  = 0.0;
+        
+		double last_erroryaw_ = 0.0;
+		double error_sumyaw_  = 0.0;
+
+		double last_erroroll_ = 0.0;
+		double error_sumroll_  = 0.0;
+		
+		double last_errorpitch_ = 0.0;
+		double error_sumpitch_  = 0.0;
+		
+		double start_time_ = 0.0;
 		double last_time_  = 0.0;
         double timenow_;
 
-        
-		ros::NodeHandle nodeHandle_;
-		ros::Subscriber poseSub_;
+
+		
 		
        
 
@@ -55,7 +99,12 @@ namespace quadrotor_controller
 		double pose_x_;
 		double pose_y_;
 		double pose_z_;
-
+        double yaw_;
+		double pitch_;
+		double roll_;
+		double dyaw_;
+		double dpitch_;
+		double droll_;
 
 
 
--- src/QuadRotorController.cpp
+++ src/QuadRotorController.cpp
@@ -12,8 +12,13 @@ namespace quadrotor_controller
 
         //--subscribers--//
         poseSub_ = nodeHandle_.subscribe("/pose", 10,&QuadRotorController::poseCallback, this);
+        eulerSub_= nodeHandle_.subscribe("/euler",10,&QuadRotorController::eulerCallback,this);
+
+        velPub_  = nodeHandle_.advertise<geometry_msgs::Twist>("/cmd_vel",10);
         ROS_INFO("Successfully launched node.");
-        timenow_ = ros::Time::now().toSec();
+        
+        
+       
         
        
 
@@ -37,21 +42,94 @@ namespace quadrotor_controller
          pose_y_ = posemsg->pose.position.y;
          pose_z_ = posemsg->pose.position.z;
 
+         
     }
+    
+    void QuadRotorController::eulerCallback(const geometry_msgs::Vector3StampedConstPtr &eulermsg)
+    {
+        yaw_   = eulermsg->vector.z;
+        pitch_ = eulermsg->vector.y;
+        roll_  = eulermsg->vector.x;
+        timenow_ = ros::Time::now().toSec();
+        QuadRotorController::pidController(pose_z_,timenow_);
+        velPub_.publish(velmsg_);
+
+    }
+
+
     void QuadRotorController::pidController(double pose_z,double time_now)
     {
         double dt = time_now - last_time_;
         if (dt == 0) 
         {
-            thrust_ = 0;
+            thrust_ = 0.0;
         }
         last_time_ = time_now ;
-        double error_z   = pose_z_ - pose_z;
-        error_sum_ += (error_z*dt) ;
-        double derror_z  = error_z - last_error_;
-        last_error_ = error_z;
+        double error_z   = pos_z_target - pose_z_;
+        error_sumz_ += (error_z*dt) ;
+        double derror_z  = error_z - last_errorz_;
+        last_errorz_ = error_z;
+        double ddz = (kpz_*error_z)+(kdz_*derror_z);
 
-        thrust_ = (kpz_*error_z)+(kdz_*derror_z);
+        double error_y   = pos_y_target - pose_y_;
+        error_sumy_ += (error_y*dt) ;
+        double derror_y  = error_y - last_errory_;
+        last_errory_ = error_y;
+        double ddy = (kpy_*error_y)+(kdy_*derror_y);
+
+        double error_x   = pos_x_target - pose_x_;
+        error_sumx_ += (error_x*dt) ;
+        double derror_x  = error_x - last_errorx_;
+        last_errorx_ = error_x;
+        double ddx = (kpx_*error_x)+(kdx_*derror_x);
+
+        double roll_des   =(1/9.8)*((ddx*yaw_target)-ddy); 
+        double pitch_des  =(1/9.8)*(ddx+(ddy*yaw_target));
+
+        thrust_ = ddz;
+
+        double error_roll   = roll_des - roll_;
+        error_sumroll_ += (error_roll*dt) ;
+        double derror_roll  = error_roll - last_erroroll_;
+        last_erroroll_ = error_roll;
+        double ddroll = (kp_roll*error_roll)+(kd_roll*derror_roll);
+
+        double error_pitch   = pitch_des - pitch_;
+        error_sumpitch_ += (error_pitch*dt) ;
+        double derror_pitch  = error_pitch - last_errorpitch_;
+        last_errorpitch_ = error_pitch;
+        double ddpitch = (kp_pitch*error_pitch)+(kd_pitch*derror_pitch);
+
+        double error_yaw   = yaw_target - yaw_;
+        error_sumyaw_ += (error_yaw*dt) ;
+        double derror_yaw = error_yaw - last_erroryaw_;
+        last_erroryaw_ = error_yaw;
+        double ddyaw = (kp_yaw*error_yaw)+(kd_yaw*derror_yaw);
+
+        momentx_ = ddroll;
+        momenty_ = ddpitch;
+        momentz_ = ddyaw;
+
+
+
+
+
+
+        
+        
+        
+
+
+
+
+        velmsg_.linear.x = ddx;
+        velmsg_.linear.y = ddy;
+        velmsg_.linear.z = thrust_;
+        velmsg_.angular.x = 0.0;
+        velmsg_.angular.y = 0.0;
+        velmsg_.angular.z = 0.0;
+
+        
        
 
     }
